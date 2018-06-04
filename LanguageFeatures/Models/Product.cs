1eb9ca6553d7c9bd667833df4747d8e81b8cce7a
Ando Ghulyan <andoghulyan@outlook.com>
2018-06-04 21:23:41 +0400

    Added  new Projects

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get; } = true;
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Category = "Water Craft",
+                Price = 275M
+            };
+            Product lifejacket = new Product
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
+
+            kayak.Related = lifejacket;
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+
+    public class ShoppingCart : IEnumerable<Product>
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+            decimal total = 0;
+            foreach (Product prod in products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+        }
+
+        public static IEnumerable<Product> FilterByPrice(this IEnumerable<Product> productEnum, decimal minimumPrice)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if ((prod?.Price ?? 0) >= minimumPrice)
+                {
+                    yield return prod;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> FilterByName(this IEnumerable<Product> productEnum, char firstLetter)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (prod?.Name?[0] == firstLetter)
+                {
+                    yield return prod;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> Filter(this IEnumerable<Product> productEnum, Func<Product, bool> selector)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (selector(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+
+}
