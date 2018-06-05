c8d8de1074e36e98ae533ac13eb53c4b2040c70f
Ando Ghulyan <andoghulyan@outlook.com>
2018-06-05 08:25:54 +0400

    Changes in Razor project. Minor corrections.

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -11,16 +11,14 @@ namespace Razor.Controllers
     {
         public IActionResult Index()
         {
-            Product myProduct = new Product
-            {
-                ProductID = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "Watersports",
-                Price = 275M
-            };
-
-            return View(myProduct);
+            Product[] array =
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+                };
+            return View(array);
         }
     }
 }
