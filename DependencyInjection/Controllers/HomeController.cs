9095ddb7715c172989cf3385aa1ce558d700d1e3
Ando Ghulyan <andoghulyan@outlook.com>
2018-06-16 15:33:39 +0400

    Added Dependency Injection Project

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DependencyInjection.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        private ProductTotalizer totalizer;
+
+        public HomeController(IRepository repo, ProductTotalizer total)
+        {
+            repository = repo;
+            totalizer = total;
+        }
+
+        //public ViewResult Index()
+        //{
+        //    ViewBag.HomeController = repository.ToString();
+        //    ViewBag.Totalizer = totalizer.Repository.ToString();
+        //    return View(repository.Products);
+        //}
+
+        public ViewResult Index([FromServices]ProductTotalizer totalizer)
+        {
+            IRepository repository = HttpContext.RequestServices.GetService<IRepository>();
+
+            ViewBag.HomeController = repository.ToString();
+            ViewBag.Totalizer = totalizer.Repository.ToString();
+            return View(repository.Products);
+        }
+    }
+}
---
+++ DependencyInjection/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using DependencyInjection.Infrastructure;
+using DependencyInjection.Models;
+
+namespace DependencyInjection
+{
+    public class Startup
+    {
+        private IHostingEnvironment env;
+
+        public Startup(IHostingEnvironment hostEnv) => env = hostEnv;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddTransient<IRepository>(provider => {
+            //    if (env.IsDevelopment())
+            //    {
+            //        var x = provider.GetService<MemoryRepository>();
+            //        return x;
+            //    }
+            //    else
+            //    {
+            //        return new AlternateRepository();
+            //    }
+            //});
+            //services.AddTransient<IRepository, MemoryRepository>();
+            services.AddTransient<MemoryRepository>();
+            services.AddTransient<IModelStorage, DictionaryStorage>();
+            services.AddTransient<ProductTotalizer>();
+            //services.AddScoped<IRepository, MemoryRepository>();
+            services.AddSingleton<IRepository, MemoryRepository>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+
+    }
+}
