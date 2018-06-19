3c537cc2ea02331153105fb15cb57ce5ea822a95
Ando Ghulyan <andoghulyan@outlook.com>
2018-06-19 17:27:49 +0400

    UsingViewComponents project added

---
+++ UsingViewComponents/Models/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+
+namespace UsingViewComponents.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+        void AddProduct(Product newProduct);
+    }
+}
