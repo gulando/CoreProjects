d272e536949c2adf8efa01934f56b02362256db3
Ando Ghulyan <andoghulyan@outlook.com>
2018-06-22 17:29:27 +0400

    Added new Project

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcModels.Models;
+using System.Linq;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            Person person;
+            if (id.HasValue && (person = repository[id.Value]) != null)
+            {
+                return View(person);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public ViewResult Create() => View(new Person());
+
+        [HttpPost]
+        public ViewResult Create(Person model) => View("Index", model);
+    }
+}
