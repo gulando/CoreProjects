080a30d6e018f903caccadd48880e92f865e3a61
Ando Ghulyan <andoghulyan@outlook.com>
2018-06-28 16:41:15 +0400

    Added Identy Project

---
+++ Users/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Users.Models;
+
+public class AdminController : Controller
+{
+    private UserManager<AppUser> userManager;
+
+    public AdminController(UserManager<AppUser> usrMgr)
+    {
+        userManager = usrMgr;
+    }
+
+    public ViewResult Index() => View(userManager.Users);
+
+    public ViewResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            AppUser user = new AppUser
+            {
+                UserName = model.Name,
+                Email = model.Email
+            };
+
+            IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+        }
+
+        return View(model);
+    }
+
+}
---
+++ Users/Infrastructure/CustomUserValidator.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Users.Models;
+
+
+namespace Users.Infrastructure
+{
+    public class CustomUserValidator : IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            if (user.Email.ToLower().EndsWith("@example.com"))
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError
+                {
+                    Code = "EmailDomainError",
+                    Description = "Only example.com email addresses are allowed"
+                }));
+            }
+        }
+
+    }
+}
